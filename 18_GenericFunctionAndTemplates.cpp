aca49eec863496977a48bb437c1a8bb2a134140e
HunterManee <huntermanee3a33@outlook.com>
2026-04-16 13:05:10 -0400

    GenericFunction and Templates

---
+++ 18_GenericFunctionAndTemplates.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+void Swap(T& a, T& b);
+
+int main() {
+    int a = 5, b = 7;
+    cout << a << " - " << b << endl;
+    Swap<int>(a,b);
+    cout << a << " - " << b << endl;
+
+    char c = 'c', d = 'd';
+    cout << c << " - " << d << endl;
+    Swap(c,d);
+    cout << c << " - " << d << endl;
+
+}
+
+template<typename T>
+void Swap(T& a, T& b){
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
