22250dee637db6aac7918a00a9069d653c708dd6
HunterManee <huntermanee3a33@outlook.com>
2026-04-16 13:15:57 -0400

    Recursion

--- 18_GenericFunctionAndTemplates.cpp
+++ 18_GenericFunctionAndTemplates.cpp
@@ -7,7 +7,7 @@ void Swap(T& a, T& b);
 int main() {
     int a = 5, b = 7;
     cout << a << " - " << b << endl;
-    Swap<int>(a,b);
+    Swap<int>(a,b); //<int> is optional; good for readablity though
     cout << a << " - " << b << endl;
 
     char c = 'c', d = 'd';
