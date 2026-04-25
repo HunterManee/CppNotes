48795a3b4d1aca312b8bba9ecc4799f802dd64a5
HunterManee <huntermanee3a33@outlook.com>
2026-04-25 13:27:23 -0400

    Functionallity Update

--- Snippit/000_polymorphic_testbed.cpp
+++ Snippit/000_polymorphic_testbed.cpp
@@ -6,27 +6,48 @@ class BaseClass {
     public:
         virtual ~BaseClass() = default;
         virtual void function() = 0; //pure virtual
+        virtual BaseClass* clone() const = 0;
 };
 class derivedClassA : public BaseClass {
     public:
+        derivedClassA(){}
+        derivedClassA(const derivedClassA& toCopy) {
+            *this = toCopy;
+        }
+
+        BaseClass* clone() const override {
+            return new derivedClassA(*this);
+        }
+
         void function() override {
             std::cout << "From Derived Function A\n";
-        }   
+        }  
 };
 class derivedClassB : public BaseClass {
     public:
+        derivedClassB(){}
+        derivedClassB(const derivedClassB& toCopy) {
+            *this = toCopy;
+        }
+
+        BaseClass* clone() const override {
+            return new derivedClassB(*this);
+        }
+
         void function() override {
             std::cout << "From Derived Function B\n";
         } 
 };
+
 
 class TestingGrounds {
     private:
         BaseClass* BC;
 
     public:
-        TestingGrounds(BaseClass* dC)
-            : BC(dC) {}
+        TestingGrounds(const BaseClass& dC)
+            : BC(dC.clone()) {}
+
 
         ~TestingGrounds() {
             delete BC;
@@ -38,12 +59,19 @@ class TestingGrounds {
 };
 
 int main() {
-    derivedClassA dA;
-    derivedClassB dB;
-    
-    TestingGrounds tg1{&dA};
-    TestingGrounds tg2{&dB};
 
+    //Stack Test
+    derivedClassA dA;
+    TestingGrounds tg1{dA};
     tg1.testing();
+
+
+    //Heap Test
+    derivedClassB* dB = new derivedClassB();
+    TestingGrounds tg2{*dB};
+
+    delete dB;
+    dB = nullptr;
+
     tg2.testing();
 }
